e95792b0335d0cc662040e5ff6819a21640ba63f
Evgeniy <timeinvest27@gmail.com>
2019-09-21 10:41:58 +0300

    Класс для получения ответа с сервера

---
+++ ChatClientWinFormsSimberSoft/AuthResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClientWinFormsSimberSoft
+{
+    class AuthResponse
+    {
+        public string Token { get; set; }
+        public string Error { get; set; }
+    }
+}
