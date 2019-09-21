7bfc175b47f96ea75c0d8609ab6cb97971201f20
Evgeniy <timeinvest27@gmail.com>
2019-09-21 10:49:09 +0300

    Добавление полей регистрации на форму логина

--- ChatClientWinFormsSimberSoft/LoginForm.Designer.cs
+++ ChatClientWinFormsSimberSoft/LoginForm.Designer.cs
@@ -34,11 +34,21 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbPasswordForRegistration = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnRegistration = new System.Windows.Forms.Button();
+            this.tbLoginForRegistration = new System.Windows.Forms.TextBox();
+            this.tbConfirmPasswordForRegistration = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblOr = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tbLogin
             // 
-            this.tbLogin.Location = new System.Drawing.Point(12, 25);
+            this.tbLogin.Location = new System.Drawing.Point(15, 57);
             this.tbLogin.MaxLength = 12;
             this.tbLogin.Name = "tbLogin";
             this.tbLogin.Size = new System.Drawing.Size(107, 20);
@@ -46,7 +56,7 @@
             // 
             // btnEnter
             // 
-            this.btnEnter.Location = new System.Drawing.Point(12, 112);
+            this.btnEnter.Location = new System.Drawing.Point(15, 144);
             this.btnEnter.Name = "btnEnter";
             this.btnEnter.Size = new System.Drawing.Size(107, 39);
             this.btnEnter.TabIndex = 1;
@@ -57,7 +67,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(15, 41);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(107, 13);
             this.label1.TabIndex = 2;
@@ -66,7 +76,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 59);
+            this.label2.Location = new System.Drawing.Point(12, 91);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(112, 13);
             this.label2.TabIndex = 3;
@@ -74,18 +84,115 @@
             // 
             // tbPassword
             // 
-            this.tbPassword.Location = new System.Drawing.Point(12, 75);
+            this.tbPassword.Location = new System.Drawing.Point(15, 107);
             this.tbPassword.Name = "tbPassword";
             this.tbPassword.Size = new System.Drawing.Size(107, 20);
             this.tbPassword.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(16, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "АВТОРИЗАЦИЯ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(201, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "РЕГИСТРАЦИЯ";
+            // 
+            // tbPasswordForRegistration
+            // 
+            this.tbPasswordForRegistration.Location = new System.Drawing.Point(200, 100);
+            this.tbPasswordForRegistration.Name = "tbPasswordForRegistration";
+            this.tbPasswordForRegistration.Size = new System.Drawing.Size(107, 20);
+            this.tbPasswordForRegistration.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(197, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Введите Ваш пароль";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(200, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Введите Ваш Логин";
+            // 
+            // btnRegistration
+            // 
+            this.btnRegistration.Location = new System.Drawing.Point(200, 175);
+            this.btnRegistration.Name = "btnRegistration";
+            this.btnRegistration.Size = new System.Drawing.Size(107, 39);
+            this.btnRegistration.TabIndex = 7;
+            this.btnRegistration.Text = "Регистрация";
+            this.btnRegistration.UseVisualStyleBackColor = true;
+            // 
+            // tbLoginForRegistration
+            // 
+            this.tbLoginForRegistration.Location = new System.Drawing.Point(200, 57);
+            this.tbLoginForRegistration.MaxLength = 12;
+            this.tbLoginForRegistration.Name = "tbLoginForRegistration";
+            this.tbLoginForRegistration.Size = new System.Drawing.Size(107, 20);
+            this.tbLoginForRegistration.TabIndex = 6;
+            // 
+            // tbConfirmPasswordForRegistration
+            // 
+            this.tbConfirmPasswordForRegistration.Location = new System.Drawing.Point(200, 143);
+            this.tbConfirmPasswordForRegistration.Name = "tbConfirmPasswordForRegistration";
+            this.tbConfirmPasswordForRegistration.Size = new System.Drawing.Size(107, 20);
+            this.tbConfirmPasswordForRegistration.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(197, 127);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Повторите пароль";
+            // 
+            // lblOr
+            // 
+            this.lblOr.AutoSize = true;
+            this.lblOr.Location = new System.Drawing.Point(147, 24);
+            this.lblOr.Name = "lblOr";
+            this.lblOr.Size = new System.Drawing.Size(25, 13);
+            this.lblOr.TabIndex = 14;
+            this.lblOr.Text = "или";
             // 
             // LoginForm
             // 
             this.AcceptButton = this.btnEnter;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(128, 163);
+            this.ClientSize = new System.Drawing.Size(329, 235);
             this.ControlBox = false;
+            this.Controls.Add(this.lblOr);
+            this.Controls.Add(this.tbConfirmPasswordForRegistration);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbPasswordForRegistration);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnRegistration);
+            this.Controls.Add(this.tbLoginForRegistration);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.tbPassword);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -106,5 +213,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbPasswordForRegistration;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnRegistration;
+        private System.Windows.Forms.TextBox tbLoginForRegistration;
+        private System.Windows.Forms.TextBox tbConfirmPasswordForRegistration;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblOr;
     }
 }
