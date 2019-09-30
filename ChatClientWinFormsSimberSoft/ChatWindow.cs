5962618c37525a8d39d52411d8727aff21fd5195
Evgeniy <timeinvest27@gmail.com>
2019-09-30 10:41:55 +0300

    Эксперементальный класс для передачи всех данных

---
+++ ChatClientWinFormsSimberSoft/ChatWindow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClientWinFormsSimberSoft
+{
+    class ChatWindow
+    {
+        string NameRoom { get; set; }
+        string NewMessage { get; set; }
+        string ListMessages { get; set; }
+        string ListUsers { get; set; }
+        string ListRooms { get; set; }
+    }
+}
