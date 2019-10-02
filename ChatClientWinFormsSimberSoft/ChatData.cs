860ed62b9fdaface640b406f7c903ee10bfef048
Evgeniy <timeinvest27@gmail.com>
2019-10-02 18:00:05 +0300

    Добавил поля в ЧатДата

--- ChatClientWinFormsSimberSoft/ChatData.cs
+++ ChatClientWinFormsSimberSoft/ChatData.cs
@@ -7,5 +7,7 @@
         public string User { get; set; }
         public string ListUsers { get; set; }
         public string ListRooms { get; set; }
+        public string Error { get; set; }
+        public string ActionString { get; set; }
     }
 }
