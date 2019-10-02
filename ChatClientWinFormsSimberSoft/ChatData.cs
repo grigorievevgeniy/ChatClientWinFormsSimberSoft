8fc1b5477d6ed8c7f065b686eef533733c6a4a01
Evgeniy <timeinvest27@gmail.com>
2019-10-02 14:53:28 +0300

    Класс для обменна данными между сервером и клиентом

---
+++ ChatClientWinFormsSimberSoft/ChatData.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatClientWinFormsSimberSoft
+{
+    public class ChatData
+    {
+        public string Room { get; set; }
+        public string Message { get; set; }
+        public string User { get; set; }
+        public string ListUsers { get; set; }
+        public string ListRooms { get; set; }
+    }
+}
